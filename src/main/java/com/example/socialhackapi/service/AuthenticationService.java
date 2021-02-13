fba60fb781c2d72c7843ffbb23671aeea071df23
Iago Tudela <iagotudela@gmail.com>
2021-02-13 14:19:43 +0100

    itd | pushed branch with no JDBC dependency

--- src/main/java/com/example/socialhackapi/service/AuthenticationService.java
+++ src/main/java/com/example/socialhackapi/service/AuthenticationService.java
@@ -1,20 +1,10 @@
 package com.example.socialhackapi.service;
 
-import com.example.socialhackapi.repository.AuthenticationRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
-
-    private AuthenticationRepository authenticationRepository;
-
-    @Autowired
-    public AuthenticationService(AuthenticationRepository authenticationRepository) {
-        this.authenticationRepository = authenticationRepository;
-    }
-
     public String hello() {
-        return "Hello";
+        return "Dumb Hello";
     }
 }
